97de683153faa08c4bd790659757876af7000c26
Felipe <12felipexd@gmail.com>
2026-09-16 20:47:55 -0300

    16/09/2026

---
+++ media-notas.py
@@ -0,0 +1,19 @@
+alunos = [
+    {"nome": "Ana", "notas": [8, 7, 9]},
+    {"nome": "Carlos", "notas": [5, 6, 4]},
+    {"nome": "Maria", "notas": [10, 9, 8]},
+    {"nome": "João", "notas": [6, 7, 5]}
+]
+
+
+def calcular_media(notas):
+    # seu código
+    pass
+
+
+def verificar_aprovacao(media):
+    # seu código
+    pass
+
+
+# seu código principal
